50a5fbffd1f93a2d9d061bc057f3941dffc7389d
iwebker <cnanker@qq.com>
2016-07-16 14:54:28 +0800

    MoudleUser
    用户信息封装类

---
+++ src/vo/User.java
@@ -0,0 +1,48 @@
+package vo;
+
+public class User {
+	private int userId;
+	private String userName;
+	private String passWord;
+	private String nickName;
+	private String imageName;
+	private String mood;
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getPassWord() {
+		return passWord;
+	}
+	public void setPassWord(String passWord) {
+		this.passWord = passWord;
+	}
+	public String getNickName() {
+		return nickName;
+	}
+	public void setNickName(String nickName) {
+		this.nickName = nickName;
+	}
+	public String getImageName() {
+		return imageName;
+	}
+	public void setImageName(String imageName) {
+		this.imageName = imageName;
+	}
+	public String getMood() {
+		return mood;
+	}
+	public void setMood(String mood) {
+		this.mood = mood;
+	}
+	
+	
+}
